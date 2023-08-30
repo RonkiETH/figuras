ba3ed47361c11017bfbe649819612290ddac97e1
Juan Ignacio Ronquillo <ronquillojuanignacio@gmail.com>
2023-08-29 21:17:12 -0300

    Se agrega constructor y atributos

--- src/ar/edu/unlam/pb2/figuras/Rectangulo.java
+++ src/ar/edu/unlam/pb2/figuras/Rectangulo.java
@@ -1,5 +1,11 @@
 package ar.edu.unlam.pb2.figuras;
-
+	
 public class Rectangulo {
-
+	private Double base;
+	private Double altura;
+	
+	public Rectangulo (Double base, Double altura) {
+		this.base= base;
+		this.altura= altura;
+	}
 }
